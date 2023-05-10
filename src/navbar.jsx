4618c3b5d9cf03a9f9954cc3f15271a85cd20f36
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2023-05-11 00:16:20 +0530

    1stCommit

---
+++ src/navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="navbar w-full bg-gray-400 p-3 flex justify-center items-center">
+      <div className="title text-white">
+        User management Sustem (CRUD+ReactJs)
+      </div>
+    </div>
+  );
+};
+export default Navbar;
